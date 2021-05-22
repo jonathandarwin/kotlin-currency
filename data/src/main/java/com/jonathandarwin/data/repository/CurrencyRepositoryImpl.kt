73333e7c7936333a70f61ba192bb97798a146a20
Jonathan Darwin <jonathanjojo38@yahoo.com>
2021-05-22 14:18:54 +0700

    add withContext IO in repo

--- data/src/main/java/com/jonathandarwin/data/repository/CurrencyRepositoryImpl.kt
+++ data/src/main/java/com/jonathandarwin/data/repository/CurrencyRepositoryImpl.kt
@@ -10,6 +10,9 @@ import com.jonathandarwin.domain.dto.convert.ConvertResponse
 import com.jonathandarwin.domain.dto.currencies.CurrenciesItemResponse
 import com.jonathandarwin.domain.dto.currencies.CurrenciesResponse
 import com.jonathandarwin.domain.entity.CurrencyDTO
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 /**
@@ -21,38 +24,48 @@ class CurrencyRepositoryImpl @Inject constructor(
 ) : CurrencyRepository {
 
     override suspend fun convert(request: ConvertRequest): ApiResponse<ConvertResponse> {
-        val response = remoteService.convert(request.from, request.to, request.amount, RetrofitBuilder.apiKey)
-        if(response.isSuccessful){
-            return ApiResponse.Success(response.body()!!)
-        }
-        else {
-            throw Exception("Please try again.")
+        return withContext(Dispatchers.IO) {
+            val response = remoteService.convert(request.from, request.to, request.amount, RetrofitBuilder.apiKey)
+            if(response.isSuccessful){
+                ApiResponse.Success(response.body()!!)
+            }
+            else {
+                throw Exception("Please try again.")
+            }
         }
     }
 
     override suspend fun getCurrencies(): ApiResponse<CurrenciesResponse> {
-        val currencies = arrayListOf<CurrenciesItemResponse>()
-        currencies.add(CurrenciesItemResponse("Indonesian Rupiah", "IDR"))
-        currencies.add(CurrenciesItemResponse("United States Dollar", "USD"))
-        currencies.add(CurrenciesItemResponse("Japanese Yen", "JPY"))
-        currencies.add(CurrenciesItemResponse("South Korean Won", "KRW"))
-        currencies.add(CurrenciesItemResponse("Malaysian Ringgit", "MYR"))
-        currencies.add(CurrenciesItemResponse("Hong Kong Dollar", "HKD"))
-        currencies.add(CurrenciesItemResponse("Chinese Yuan", "HKD"))
-        currencies.add(CurrenciesItemResponse("Australian Dollar", "AUD"))
-        val response = CurrenciesResponse(currencies)
-        return ApiResponse.Success<CurrenciesResponse>(response)
+        return withContext(IO) {
+            val currencies = arrayListOf<CurrenciesItemResponse>()
+            currencies.add(CurrenciesItemResponse("Indonesian Rupiah", "IDR"))
+            currencies.add(CurrenciesItemResponse("United States Dollar", "USD"))
+            currencies.add(CurrenciesItemResponse("Japanese Yen", "JPY"))
+            currencies.add(CurrenciesItemResponse("South Korean Won", "KRW"))
+            currencies.add(CurrenciesItemResponse("Malaysian Ringgit", "MYR"))
+            currencies.add(CurrenciesItemResponse("Hong Kong Dollar", "HKD"))
+            currencies.add(CurrenciesItemResponse("Chinese Yuan", "HKD"))
+            currencies.add(CurrenciesItemResponse("Australian Dollar", "AUD"))
+            val response = CurrenciesResponse(currencies)
+            ApiResponse.Success<CurrenciesResponse>(response)
+        }
     }
 
     override suspend fun saveConvertCurrency(currenyDTO: CurrencyDTO): Boolean {
-        return database.currencyDAO.insert(currenyDTO) > 0
+        return withContext(IO) {
+            database.currencyDAO.insert(currenyDTO) > 0
+        }
     }
 
     override suspend fun getConvertCurrencyHistory(limit: Int): List<CurrencyDTO> {
-        return database.currencyDAO.get()
+        return withContext(IO) {
+            database.currencyDAO.get()
+        }
     }
 
     override suspend fun deleteAllHistory(): Boolean {
-        return database.currencyDAO.deleteAll() > 0
+        return withContext(IO) {
+            database.currencyDAO.deleteAll() > 0
+        }
     }
 }
